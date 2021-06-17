62930c7f525ab8f87efc954cc43544fde75c752f
ARandomProgrammer <50036343+4RandomProgrammer@users.noreply.github.com>
2021-06-17 10:49:03 -0300

    git

--- cadastro.php
+++ cadastro.php
@@ -11,30 +11,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cinema - Cadastro de Problemas</title>
 </head>
-<body>
-    <h1>Cinema top</h1>
-    <h2>Cadastro de problemas</h2>
-    <form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST">
-    <label for="cSala">Sala: </label>
-    <select name="tSala" id="cSala">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-    </select> <br><br>
-    <label for="tUrgencia">Gravidade: </label> 
-    <input type="radio" name="tUrgencia" id="cBaixa" value="baixa" ><label for="cBaixa">Baixa</label>
-    <input type="radio" name="tUrgencia" id="cMedia" value="media"> <label for="cMedia">Média</label>
-    <input type="radio" name="tUrgencia" id="cUrgente" value="urgente"> <label for="cUrgente">Urgente</label>                
-    <br><br>
-    <label for="cData">Data de início: </label><input type="date" name="tData" id="cData">
-    <br><br>
-    <label for="cDescricao">Descrição: <br></label><textarea name="tDescricao" id="cDescricao" cols="50" rows="6"></textarea>
-    </form>
+    <body>
+        <h1>Cinema top</h1>
+        <h2>Cadastro de problemas</h2>
+        <form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST">
+        <label for="cSala">Sala: </label>
+        <select name="tSala" id="cSala">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+        </select> <br><br>
+        <label for="tUrgencia">Gravidade: </label> 
+        <input type="radio" name="tUrgencia" id="cBaixa" value="baixa" ><label for="cBaixa">Baixa</label>
+        <input type="radio" name="tUrgencia" id="cMedia" value="media"> <label for="cMedia">Média</label>
+        <input type="radio" name="tUrgencia" id="cUrgente" value="urgente"> <label for="cUrgente">Urgente</label>                
+        <br><br>
+        <label for="cData">Data de início: </label><input type="date" name="tData" id="cData">
+        <br><br>
+        <label for="cDescricao">Descrição: <br></label><textarea name="tDescricao" id="cDescricao" cols="50" rows="6"></textarea>
+        </form>
+        <a href="index.php">Voltar para index</a>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device=width, initial-scale1.0">
     <title>Cinema - Gerenciamento de Problemas</title>
 </head>
-
-
+    <a href="cadastro.php">Registro de Problema</a>
+    <a href="modificar.php">Modificar status da Sala</a>
+    <a href="verNomes.php">Ver as Salas</a>
 </html>
