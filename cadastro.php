5e259f37310d82a9461c726972986911ec6e5034
ARandomProgrammer <50036343+4RandomProgrammer@users.noreply.github.com>
2021-06-17 10:32:41 -0300

    A

--- cadastro.php
+++ cadastro.php
@@ -2,13 +2,13 @@
 
 //salve
 
-?>
+?><!DOCTYPE html>
 
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device=width, initial-scale1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cinema - Cadastro de Problemas</title>
 </head>
 <body>
