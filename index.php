554aa15139c9569a42ed8a22a38803e18defd9f6
Felipe Kogus <felipekogus@gmail.com>
2021-06-17 10:25:12 -0300

    Cabeçalho index

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+
+<head><meta charset="UTF-8">
+    <meta name="viewport" content="width=device=width, initial-scale1.0">
+    <title>Cinema - Gerenciamento de Problemas</title>
+</head>
+
+
+</html>
