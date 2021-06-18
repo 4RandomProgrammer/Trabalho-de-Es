4ef79989d317b4f097d9bdfd8b9d315c879042b9
Jade Manzur <50030683+jdmanzur@users.noreply.github.com>
2021-06-18 16:07:03 -0300

    remocao talvez funcione

--- remocao.php
+++ remocao.php
@@ -1,6 +1,37 @@
 <?php
+function MyAutoload($className) {
+        $extension =  spl_autoload_extensions();
+        require_once (__DIR__ . '/' . $className . $extension);
+    }
 
-?><!DOCTYPE html>
+    spl_autoload_extensions('.class.php'); // quais extensões iremos considerar
+    spl_autoload_register('MyAutoload');
+    
+    $c = new Controle();
+
+    for ($x = 0; $x <= 10; $x++) {
+        $query = 'SELECT descricao, data, urgencia, numero FROM problema WHERE numero = <?=$x?>';
+        
+        $selecao = $c->selectBD($query);
+        
+        while($linha = mysqli_fetch_array($selecao)){
+        ?>
+        
+        <ul>
+            <li>Sala <?=$linha['numero']?> (<?=$linha['numacentos']?> lugares) - <font color=<?php echo $cor ?>><?=$status?></font> </li>
+        </ul>
+
+        <?php
+        } 
+    
+    } 
+
+    
+
+?>
+
+
+<!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
