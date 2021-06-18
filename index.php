aeb407d8e008c3c47d81a8949fc365ccccb8192c
ARandomProgrammer <50036343+4RandomProgrammer@users.noreply.github.com>
2021-06-18 10:15:15 -0300

    update no index

--- index.php
+++ index.php
@@ -7,11 +7,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 </head>
     <nav class="navbar navbar-dark bg-dark">
-    <a class="navbar-brand" href="#"><h1>Sistema de Registro de Problemas</h1></a>
+    <a class="navbar-brand" href="#">Sistema de Registro de Problemas</a>
     </nav>
-    
-    <a type="button" class="btn btn-outline-dark" href="cadastro.php">Registro de Problema</a>
+
+    <a type="button" class="btn btn-outline-dark" href="cadastro.php">Registrar de Problema</a>
+
     <a type="button" class="btn btn-outline-dark" href="modificar.php">Modificar status da Sala</a>
+
     <a type="button" class="btn btn-outline-dark" href="verNomes.php">Ver as Salas</a>
     
 </html>
