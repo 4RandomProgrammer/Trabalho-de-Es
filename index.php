b64ba6bff711ea6010c62b59a09ad12e27704edb
ARandomProgrammer <50036343+4RandomProgrammer@users.noreply.github.com>
2021-06-18 09:06:28 -0300

    Css só de meme

--- index.php
+++ index.php
@@ -4,8 +4,14 @@
 <head><meta charset="UTF-8">
     <meta name="viewport" content="width=device=width, initial-scale1.0">
     <title>Cinema - Gerenciamento de Problemas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 </head>
-    <a href="cadastro.php">Registro de Problema</a>
-    <a href="modificar.php">Modificar status da Sala</a>
-    <a href="verNomes.php">Ver as Salas</a>
+    <nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#"><h1>Sistema de Registro de Problemas</h1></a>
+    </nav>
+    
+    <a type="button" class="btn btn-outline-dark" href="cadastro.php">Registro de Problema</a>
+    <a type="button" class="btn btn-outline-dark" href="modificar.php">Modificar status da Sala</a>
+    <a type="button" class="btn btn-outline-dark" href="verNomes.php">Ver as Salas</a>
+    
 </html>
